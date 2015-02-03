82196d4290d076c4b2fc9e7b5b61d6e6cd4ddb8c
moro <moronatural@gmail.com>
2015-02-03 19:01:29 +0900

    Implement

--- lib/respond_only_supported.rb
+++ lib/respond_only_supported.rb
@@ -1,5 +1,32 @@
 require "respond_only_supported/version"
 
 module RespondOnlySupported
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class AcceptableFormats
+    def initialize(formats)
+      @formats = formats
+    end
+
+    def before(controller)
+      unless acceptable?(controller.request.formats)
+        controller.head(:not_acceptable)
+      end
+    end
+
+    private
+
+    def acceptable?(requested_formats)
+      requested_formats.any? {|type| type.ref == '*/*' } ||
+      requested_formats === @formats
+    end
+  end
+
+  module ClassMethods
+    def respond_only(*formats)
+      options = formats.extract_options!
+
+      before_action AcceptableFormats.new(formats), options
+    end
+  end
 end
--- spec/respond_only_supported_spec.rb
+++ spec/respond_only_supported_spec.rb
@@ -1,9 +1,43 @@
+require 'respond_only_supported'
+
 RSpec.describe FooController, type: :controller do
+  before(:all) do
+    FooController.class_eval do
+      include RespondOnlySupported
+
+      respond_only :html
+    end
+  end
+
   context 'GET hi / simple access (text/xhtml by default)' do
     specify 'returns 200' do
       get :hi
 
       expect(response).to be_success
+    end
+  end
+
+  context 'GET hi with Accept: */*' do
+    before do
+      request.accept = '*/*'
+    end
+
+    specify 'returns 200' do
+      get :hi
+
+      expect(response).to be_success
+    end
+  end
+
+  context 'GET hi with Accept: image/png' do
+    before do
+      request.accept = 'image/png'
+    end
+
+    specify 'returns 406' do
+      get :hi
+
+      expect(response.status).to eq 406
     end
   end
 end
